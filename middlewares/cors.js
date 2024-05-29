a8fee297fbb178afcbd619ddfd7de7ff3b87d536
Ch0c0latka <149702725+Ch0c0latka@users.noreply.github.com>
2024-05-29 22:03:44 +0300

    Comment to commit

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,28 +1,16 @@
 const allowedCors = [
   'https://practicum.yandex.ru',
   'https://students-projects.ru',
-  'http://localhost:3000'
+  'localhost:3000'
 ];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    
+    const { origin } = req.headers; 
     if (allowedCors.includes(origin)) { 
-        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Origin', origin); 
     }
     
-    next();
+    next(); 
 }
 
-
-  app.use(
-    cors, 
-    bodyParser.json(),
-    express.static(path.join(__dirname, 'public')),
-    mainRoute,
-    gamesRouter
-  )
-
-  module.exports = {
- cors
-  }
+module.exports = cors;
